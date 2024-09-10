cf1123799d8a187b1bbecfc272e54e844f6c7a0a
Fernando Victor <fernando00sjp@hotmail.com>
2024-09-10 09:41:42 -0300

    refactor search playlists

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,29 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
-import {z} from 'zod';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
 const prisma = new PrismaClient();
 
 app.get('/playlist', async (request, reply) => {
-    const playlists = await prisma.playlists.findMany();
+    const playlists = await prisma.playlists.findMany({
+        include: {
+            videos: true,
+        },
+    });
+    return reply.send(playlists);
 });
 
 app.post('/video', async (request, reply) => {
     const createPlaylistSchema = z.object({
         src: z.string(),
-        description: z.string(),
-        link: z.string(),
+        description: z.string()
     }); 
-    const { src, description, link } = createPlaylistSchema.parse(request.body);
+    const { src, description } = createPlaylistSchema.parse(request.body);
     const playlist = await prisma.playlists.create({
         data: {
-            src, description, link
+            src, description
         },
     });
     return reply.send(playlist).status(201);    
-}); 
-
-
-app.listen({
-    host: '0.0.0',
-    port: process.env.PORT? Number(process.env.PORT) : 3333,
-}).then(()=>{
-    console.log('Server is running on port 3333');
-})
+});
